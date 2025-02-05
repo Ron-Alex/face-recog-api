6c2dd240a2491ec63cea5bf10b6ed01b030fe2e4
Ron Alex <ronmathewalx@gmail.com>
2025-02-05 01:16:51 -0600

    Adding database

--- server.js
+++ server.js
@@ -29,11 +29,8 @@ app.use(cors());
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        port: 5432,
-        password: 'test',
-        database: 'smartbrain' 
+        connectionString: process.env.DATABASE_URL,
+        ssl:true
     }
 });
 
